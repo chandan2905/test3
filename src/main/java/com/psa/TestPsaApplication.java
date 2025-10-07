33f9db2dcb5a269d96ca02b843eae8a2ad972f4f
Chandan2905 <chandankumar2905@gmail.com>
2025-10-07 19:59:37 +0530

    First commit

---
+++ src/main/java/com/psa/TestPsaApplication.java
@@ -0,0 +1,14 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaApplication {
+
+	public static void main(String[] args) {
+		int x=10;
+		SpringApplication.run(TestPsaApplication.class, args);
+	}
+
+}
